fb141e7dcd9efd31d85dd841522b260274568e1d
unknown <flucchesi@kolektor.com.ar>
2022-08-10 14:16:53 -0300

    commit  ejercicio 09-tupla-clientes

---
+++ valoracion-final-modulo/09-tupla-clientes.py
@@ -0,0 +1,11 @@
+historial5 = (8520, 9510, 7530, 3570, 1590)
+
+def valorMaximoGastada(historial5):
+    max_value = max(historial5)
+    print('Maximum value:', max_value)
+    return max_value
+
+print('Comienzo del programa que ejecuta la funcion')
+print('funcion')
+valorMaximoGastada(historial5)
+print('fin de la funcion')
