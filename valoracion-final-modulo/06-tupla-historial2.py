5b3726eefe9f58e9c9c3395843620ca0fa1e057f
Erick López <erick.keyer@gmail.com>
2022-08-14 00:53:16 -0300

    Commiteando cambios en el ejercicio 06 de python

--- valoracion-final-modulo/06-tupla-historial2.py
+++ valoracion-final-modulo/06-tupla-historial2.py
@@ -1,4 +1,8 @@
 historial2 = (23500,5960,2300,10200,8900)
+montoTotal = 0
+
+for monto in historial2:
+    montoTotal += monto
 
 def atencionFrida(facturas):
     contador = 0
@@ -7,4 +11,5 @@ def atencionFrida(facturas):
             contador += 1
     return contador
 
+print("El monto total gastado a lo largo del tiempo por la atención de “Frida” es:",montoTotal)
 print("Cantidad de gastos superiores a 5000:",atencionFrida(historial2))
